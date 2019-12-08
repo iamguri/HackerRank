45cd2e5835eb13ef7b58e5f25c0fab85bbd9a380
iamguri <gurjeetbhari@gmail.com>
2019-12-08 19:20:15 +0530

    Update 3-A-Very-Big-Sum.cpp

--- Easy/3-A-Very-Big-Sum.cpp
+++ Easy/3-A-Very-Big-Sum.cpp
@@ -1,5 +1,4 @@
 https://www.hackerrank.com/challenges/a-very-big-sum/problem
-
 Problem:
 Calculate and print the sum of the elements in an array, 
 keeping in mind that some of those integers may be quite large.
