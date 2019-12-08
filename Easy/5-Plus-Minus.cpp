99e35c02edd25b0b93178188cffe17ab40227a4c
iamguri <gurjeetbhari@gmail.com>
2019-12-08 19:17:58 +0530

    Update 5-Plus-Minus.cpp

--- Easy/5-Plus-Minus.cpp
+++ Easy/5-Plus-Minus.cpp
@@ -8,6 +8,16 @@ ration would be:
 1/5=0.20000 (zero)
 2/5=0.40000 (positive)
 
+Sample Input
+
+6
+-4 3 -9 0 4 1         
+Sample Output
+
+0.500000
+0.333333
+0.166667    
+    
 Solution:
 #include <bits/stdc++.h>
 
