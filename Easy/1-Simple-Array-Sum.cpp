9ca16057fd52437281cd6ea274ea3145431e7391
iamguri <gurjeetbhari@gmail.com>
2019-12-08 19:13:59 +0530

    Update 1-Simple-Array-Sum.cpp

--- Easy/1-Simple-Array-Sum.cpp
+++ Easy/1-Simple-Array-Sum.cpp
@@ -5,6 +5,13 @@ Given an array of integers, find the sum of its elements.
 
 For example, if the array ar=[1,2,3], 1+2+3=6, so return 6.
 
+Sample Input
+
+6
+1 2 3 4 10 11
+Sample Output
+
+31
 
 Solution:
 #include <iostream>
