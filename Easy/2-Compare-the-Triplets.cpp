e573164f3883b83e4b54e4bf485ad3592d2500d3
iamguri <gurjeetbhari@gmail.com>
2019-12-08 19:19:41 +0530

    Update 2-Compare-the-Triplets.cpp

--- Easy/2-Compare-the-Triplets.cpp
+++ Easy/2-Compare-the-Triplets.cpp
@@ -1,5 +1,4 @@
 https://www.hackerrank.com/challenges/compare-the-triplets/problem
-
 Problem:
 Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale 
 from 1 to 100 for three categories: problem clarity, originality, and difficulty.
