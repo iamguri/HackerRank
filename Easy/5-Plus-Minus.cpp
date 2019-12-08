da72701d7525c43ef98e1a9c94355b2643abdc84
iamguri <gurjeetbhari@gmail.com>
2019-12-08 19:09:25 +0530

    Update and rename 5. Plus Minus to 5-Plus-Minus.cpp

--- Easy/5-Plus-Minus.cpp
+++ Easy/5-Plus-Minus.cpp
@@ -1,3 +1,14 @@
+https://www.hackerrank.com/challenges/plus-minus/problem
+Peoblem:
+Given an array of integers, calculate the fractions of its elements that are positive, 
+negative, and are zeros. Print the decimal value of each fraction on a new line.
+arr = [-2,-1,0,1,2] here are 5 elements.
+ration would be:
+2/5=0.40000 (negative)
+1/5=0.20000 (zero)
+2/5=0.20000 (positive)
+
+Solution:
 #include <bits/stdc++.h>
 
 using namespace std;
