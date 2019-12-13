bc069134fac8d093c57a6fc0b5507693dec8e1c3
iamguri <gurjeetbhari@gmail.com>
2019-12-13 18:24:59 +0530

    Update 7-Time-Conversion.cpp

--- Easy/7-Time-Conversion.cpp
+++ Easy/7-Time-Conversion.cpp
@@ -1,3 +1,16 @@
+Problem:
+Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+
+Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock. 
+Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock.
+Sample Input 0
+
+07:05:45PM
+Sample Output 0
+
+19:05:45
+Solution: 
+
 #include <bits/stdc++.h>
 
 using namespace std;
